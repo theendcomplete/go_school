9f48257ab024e84a45931d5362fcf857644693c4
Nikita Ratnikov <nikkie@evrone.com>
2021-06-29 09:37:58 +0300

    Optimize memory usage

--- remover.rb
+++ remover.rb
@@ -1,25 +1,22 @@
 # frozen_string_literal: true
 
-def remove_spaces(chars = [])
+def remove_spaces(chars_array = [])
   i = 0
-  while chars[i] == ' '
-    chars = chars[1...]
-    i += 1
-  end
-  chars = chars[...-1] while chars[chars.length - 1] == ' '
+  chars_array.delete_at(i) while chars_array[i] == ' '
+  chars_array.delete_at(-1) while chars_array[-1] == ' '
 
-  while i < (chars.length - 1)
-    if (chars[i] == ' ') && chars[i + 1] == ' '
-      chars = (chars[...i] + chars[i + 1...])
+  while i < (chars_array.length - 1)
+    if (chars_array[i] == ' ') && chars_array[i + 1] == ' '
+      chars_array.delete_at(i)
     else
       i += 1
     end
   end
-  chars
+  chars_array.join
 end
 
 puts 'Please enter string to clean up'
 
 str = gets.chomp
 puts "Your string: \n'#{str}'"
-puts "Cleaned string: \n'#{remove_spaces(str)}'"
+puts "Cleaned string: \n'#{remove_spaces(str.chars)}'"
